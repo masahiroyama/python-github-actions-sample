f46e44e909a0983c06f3191f60d7f27903d1178b
Masahiro Yamaura <tbpcsemy@gmail.com>
2023-04-08 18:21:14 +0900

    add task addition feature draft

---
+++ tasklist/mgr/task_mgr.py
@@ -0,0 +1,40 @@
+"""Task Manager class."""
+
+import os
+import json
+
+
+class TaskManager(object):
+    def __init__(self, data_path):
+        if not os.path.exists(data_path):
+            self.create_data_file(data_path)
+
+        self.read_data(data_path)
+
+    def read_data(self, data_path):
+        with open(data_path) as f:
+            self.data = json.load(f)
+
+    def write_data(self, data_path):
+        with open(data_path, "w") as f:
+            json.dump(self.data, f, indent=4)
+
+    def create_data_file(self, data_path):
+        dir_path = os.path.dirname(data_path)
+
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+
+        with open(data_path, "w") as f:
+            json.dump({}, f, ensure_ascii=False)
+
+    def add_task(self, name, due_date=None, priority=None):
+        task_id = self.data["max_id"] + 1
+        self.data["tasks"].append(
+            {
+                "id": task_id,
+                "name": name,
+                "due_date": due_date,
+                "priority": priority,
+            }
+        )
--- tasklist/tasklist.py
+++ tasklist/tasklist.py
@@ -1,14 +1,23 @@
 """tasklist main file as sample application."""
+import os
 import argparse
 
 import tasklist
+from .mgr.task_mgr import TaskManager
 
-parser = argparse.ArgumentParser(description='Task List')
+parser = argparse.ArgumentParser(description="Task List")
 
-parser.add_argument('-a', '--add', help='add task', metavar='TASK NAME')
-parser.add_argument('-l', '--list', help='list tasks', action='store_true')
+parser.add_argument("-a", "--add", help="add task", metavar="TASK NAME")
+parser.add_argument("-l", "--list", help="list tasks", action="store_true")
 parser.add_argument(
-    '-v', '--version', help='show version', action='store_true'
+    "-f",
+    "--file",
+    help="specify task data path",
+    metavar="TASK DATA FILE",
+    default="$HOME/.tl/task.json",
+)
+parser.add_argument(
+    "-v", "--version", help="show version", action="store_true"
 )
 
 args = parser.parse_args()
@@ -16,6 +25,14 @@ args = parser.parse_args()
 
 def main():
     """Tasklist main method."""
+
     if args.version:
-        print('Version: {}'.format(tasklist.__version__))
+        print("Version: {}".format(tasklist.__version__))
         return
+
+    data_path = os.path.expandvars(args.file)
+    tmgr = TaskManager(data_path)
+
+    if args.add:
+        tmgr.add_task(args.add)
+        tmgr.write_data(data_path)
