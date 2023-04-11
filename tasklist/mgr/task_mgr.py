702801ef0c9c095c91a854484a1344d4c66b634e
Masahiro Yamaura <tbpcsemy@gmail.com>
2023-04-11 18:50:23 +0900

    Fix 'date' error for JSON

--- tasklist/mgr/task_mgr.py
+++ tasklist/mgr/task_mgr.py
@@ -19,8 +19,14 @@ class TaskManager(object):
             self.data = json.load(f)
 
     def write_data(self, data_path):
+        def custom_default(obj):
+            if isinstance(obj, datetime.date):
+                return obj.strftime('%Y/%m/%d')
+
+            raise TypeError("Type {} not serializable.".format(type(obj)))
+
         with open(data_path, "w") as f:
-            json.dump(self.data, f, indent=4)
+            json.dump(self.data, f, indent=4, default=custom_default)
 
     def create_init_data_file(self, data_path):
         dir_path = os.path.dirname(data_path)
@@ -33,13 +39,15 @@ class TaskManager(object):
 
     def add_task(self, name, due_date=None, priority=None):
         task_id = self.data["max_id"]
+
+        if due_date is not None:
+            due_date = datetime.datetime.strptime(due_date, "%Y/%m/%d").date()
+
         self.data["tasks"].append(
             {
                 "id": task_id,
                 "name": name,
-                "due_date": datetime.datetime.strptime(
-                    due_date, "%Y/%m/%d"
-                ).date(),
+                "due_date": due_date,
                 "priority": priority,
             }
         )
